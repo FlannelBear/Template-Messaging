03ee0cc1b0533a09d353f954a880f0eaedc35fd7
Flannel Bear <josh.flannelbear@gmail.com>
2018-08-20 14:45:36 -0500

    write generateMessage method to serve as a road map for the solution

--- server/modules/template.class.js
+++ server/modules/template.class.js
@@ -3,7 +3,19 @@ class Template{
         this.template = template;
     }
 
-    
+    generateMessage(guest, company){
+        // Create an array from the template
+        const templateArray = this.template.split(" ");
+        // Seperate punctuation from words
+        // templateArray = this.preservePunc(templateArray);
+        // Replace keys with the corresponding guest and company values
+        // messageArray = this.placeValues(templateArray, guest, company);
+        // Place punctuation in correct positions
+        // messageArray = this.placePunc(messageArray);
+        // Join array into final message string and return message
+        // return messageArray.join(' ');
+    }
+
 }
 
 module.exports = Template;
