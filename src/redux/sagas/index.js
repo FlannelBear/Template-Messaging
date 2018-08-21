e8a6c79cbcd5e2a00c4086bcb3f82ff39255fbec
Flannel Bear <josh.flannelbear@gmail.com>
2018-08-21 12:31:15 -0500

    complete sagas

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -6,11 +6,11 @@ import { getGuests,
          getMessage, 
          postMessage, 
          postTemplate } from '../requests/requests.js';
-import rootSaga from '../../../../../../kipsu-exercise/template-message/src/redux/sagas/index.js';
 
 function* guests(){
     try{
-
+        const guests = yield getGuests();
+        yield put({type: 'STORE_GUESTS', payload: guests});
     }catch(error){
         console.log("Error in guests saga: ", error);
     }
@@ -18,7 +18,8 @@ function* guests(){
 
 function* companies(){
     try{
-
+        const companies = yield getCompanies();
+        yield put({type: 'STORE_COMPANIES', payload: companies});
     }catch(error){
         console.log("Error in companies saga: ", error);
     }
@@ -26,7 +27,8 @@ function* companies(){
 
 function* message(){
     try{
-
+        const message = yield getMessage();
+        yield put({type: 'STORE_MESSAGE', payload: message});
     }catch(error){
         console.log("Error in message saga: ", error);
     }
@@ -34,23 +36,26 @@ function* message(){
 
 function* templates(){
     try{
-
+        const templates = yield getTemplates();
+        yield put({type: 'STORE_TEMPLATES', payload: templates});
     }catch(error){
         console.log("Error in templates saga: ", error);
     }
 }
 
-function* templateChoices(){
+function* templateChoices(action){
     try{
-
+        yield postMessage(action.payload);
+        yield put({type: 'FETCH'});
     }catch(error){
         console.log("Error in templateChoices saga: ", error);
     }
 }
 
-function* newTemplate(){
+function* newTemplate(action){
     try{
-
+        yield postTemplate(action.payload);
+        yield put({type: 'FETCH'});
     }catch(error){
         console.log("Error in newTemplate saga: ", error);
     }
