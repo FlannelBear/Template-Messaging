3c4ef76b855baaf6f53e9a694383e6d688957617
Flannel Bear <josh.flannelbear@gmail.com>
2018-08-21 11:55:35 -0500

    preliminary GUI styles, comment planned components

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-header">
+          <h3>Templated Messaging</h3>
+        </div>
+        {/* <GenerateMessage />
+        <AddTemplate /> */}
       </div>
     );
   }
